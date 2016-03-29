0df99986bcbab9142ce632ef75d56428ef48b7b2
kmiller <keith.miller@activision.com>
2016-03-29 10:22:04 -0700

    Temp commit to reset project

--- authservice/src/main/java/com/singlemalt/googleplay/auth/googleplayauth/AuthService.java
+++ authservice/src/main/java/com/singlemalt/googleplay/auth/googleplayauth/AuthService.java
@@ -1,5 +1,8 @@
 package com.singlemalt.googleplay.auth.googleplayauth;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.util.Log;
 
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -12,6 +15,7 @@ import com.google.android.gms.common.ConnectionResult;
  */
 public class AuthService {
     private static final String TAG = AuthService.class.getSimpleName();
+    static final int REQUEST_CODE_RECOVER_PLAY_SERVICES = 1001;
 
     public static final String[] STATUS_VALUES = new String[]
             { "Working", "Success", "Failure", "Cancel"};
@@ -41,11 +45,7 @@ public class AuthService {
     public void init() {
         Log.d(TAG, "Starting");
 
-        ConnectionResult result = new ConnectionResult(
-                GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(UnityPlayer.currentActivity.getApplicationContext())
-        );
-
-        Log.d(TAG, "Connection result: "+result.toString());
+        checkGooglePlaySevices(UnityPlayer.currentActivity);
     }
 
     public void onPause() {
@@ -55,6 +55,8 @@ public class AuthService {
 
     public void onResume() {
         Log.d(TAG, "onResume");
+
+        checkGooglePlaySevices(UnityPlayer.currentActivity);
     }
 
     public String getPlayerName() {
@@ -96,25 +98,51 @@ public class AuthService {
         Log.d(TAG, String.format("checkStatus: loginStatus %s", loginStatus));
 
 
-        if(loginStatus.equals(STATUS_VALUES[0])) {
+        if (loginStatus.equals(STATUS_VALUES[0])) {
             Log.d(TAG, "not ready to update unity");
-        } else if(loginStatus.equals(STATUS_VALUES[1])) {
+        } else if (loginStatus.equals(STATUS_VALUES[1])) {
             anonymous = false;
 
             Log.d(TAG, "login success");
             UnityPlayer.UnitySendMessage("Main Camera", "LoginResult", STATUS_VALUES[1]);
-        } else if(loginStatus.equals(STATUS_VALUES[3])) {
+        } else if (loginStatus.equals(STATUS_VALUES[3])) {
             anonymous = true;
             player = null;
 
             Log.d(TAG, "login cancelled");
             UnityPlayer.UnitySendMessage("Main Camera", "LoginResult", STATUS_VALUES[3]);
-        } else if(loginStatus.equals(STATUS_VALUES[2])) {
+        } else if (loginStatus.equals(STATUS_VALUES[2])) {
             anonymous = true;
             player = null;
 
             Log.e(TAG, "login failed");
             UnityPlayer.UnitySendMessage("Main Camera", "LoginResult", STATUS_VALUES[2]);
         }
+    }
+
+    public static boolean checkGooglePlaySevices(final Activity activity) {
+        final int googlePlayServicesCheck = GoogleApiAvailability.getInstance()
+                .isGooglePlayServicesAvailable(activity.getApplicationContext());
+
+        final DialogInterface.OnCancelListener listener = new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                activity.finish();
+            }
+        };
+
+        switch (googlePlayServicesCheck) {
+            case ConnectionResult.SUCCESS:
+                return true;
+            case ConnectionResult.SERVICE_DISABLED:
+            case ConnectionResult.SERVICE_INVALID:
+            case ConnectionResult.SERVICE_MISSING:
+            case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+                Dialog dialog = GoogleApiAvailability.getInstance()
+                        .getErrorDialog(activity, googlePlayServicesCheck, REQUEST_CODE_RECOVER_PLAY_SERVICES, listener);
+
+                dialog.show();
+        }
+        return false;
     }
 }
